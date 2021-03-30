f0f85287c500b82a201e3eff22c332371b1e6d50
Oragoss <interactiveemerald@gmail.com>
2021-03-29 23:55:54 -0500

    Calculations work, need to put them in the UI

--- Assets/Scripts/Battle.cs
+++ Assets/Scripts/Battle.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -56,50 +54,13 @@ namespace Assets.Scripts
 
         #endregion
 
-        #region Stats
-
         List<Combatant> allyCombatants = new List<Combatant>();
         List<Combatant> enemyCombatants = new List<Combatant>();
-
-        //TODO: Generalize these
-        int numberOfUngor = 0;
-        int ungorAttackModifier = 5;
-        int ungorAttackDamage = 6;
-
-        int numberOfGor = 0;
-        int gorAttackModifier = 6;
-        int gorAttackDamage = 7;
-
-        int numberOfBestigor = 0;
-        int bestigorAttackModifier = 6;
-        int bestigorAttackDamage = 15;
-
-        int numberOfGuards = 0;
-        int guardAttackModifier = 4;
-        int guardAttackDamage = 5;
-
-        int numberOfCommoners = 0;
-        int commonerAttackModifier = 4;
-        int commonerAttackDamage = 5;
-
-        int numberOfDretches = 0;
-        int dretchAttackModifier = 2;
-        int dretchAttackDamage = 3;
-
-        int numberOfMaws = 0;
-        int mawAttackModifier = 4;
-        int mawAttackDamage = 11;
-
-        int numberOfHezrou = 0;
-        int hezrouAttackModifier = 7;
-        int hezrouAttackDamage = 15;
-
-        #endregion
 
         private void Start()
         {
             instructions.text = "";
-            questionsPanel.gameObject.SetActive(false); 
+            questionsPanel.gameObject.SetActive(false);
             battlePanel.gameObject.SetActive(false);
             welcomePage.gameObject.SetActive(true);
 
@@ -110,10 +71,8 @@ namespace Assets.Scripts
             addEnemyCombatantButton.gameObject.SetActive(false);
             doneWithEnemiesButton.gameObject.SetActive(false);
 
-            //TODO: Get a sample working
-            //BattleInitiator.SampleBattle();
+            SampleBattle();
         }
-
 
         public void Initiate()
         {
@@ -133,7 +92,8 @@ namespace Assets.Scripts
             var newDamage = int.Parse(attackDamageTextField.GetComponent<InputField>().text);
             var newNumberOfAttacks = int.Parse(numberOfAttacksTextField.GetComponent<InputField>().text);
 
-            allyCombatants.Add(new Combatant() {
+            allyCombatants.Add(new Combatant()
+            {
                 Name = newName,
                 Enemy = false,
                 ArmorClass = newAC,
@@ -194,8 +154,56 @@ namespace Assets.Scripts
 
         public void CalculateBattleResults()
         {
-            //todo: eek
+            var random = new System.Random(Guid.NewGuid().GetHashCode());
 
+            for (int x = 0; x < allyCombatants.Count; x++)
+            {
+                if(allyCombatants[x].Health > 0)
+                {
+                    List<Combatant> livingPoolOfEnemies = enemyCombatants.Where(e => e.Health > 0).ToList();
+
+                    var attackRoll = random.Next(0, 19) + allyCombatants[x].AttackModifier;
+                    var randomEnemy = enemyCombatants[random.Next(0, enemyCombatants.Count)];
+                    
+
+                    if (attackRoll >= randomEnemy.ArmorClass)
+                    {
+                        randomEnemy.Health -= allyCombatants[x].AttackDamage;
+                        if (randomEnemy.Health <= 0)
+                            Debug.Log($"{randomEnemy.Name} has been slain!");
+                        else
+                            Debug.Log($"{randomEnemy.Name} took {allyCombatants[x].AttackDamage} damaage from {allyCombatants[x].Name}! He now has {randomEnemy.Health} health.");
+                    }
+                    else
+                        Debug.Log($"{randomEnemy.Name} was not hit by {allyCombatants[x].Name}");
+                }
+                else
+                    Debug.Log($"{allyCombatants[x].Name} is slain...");
+            }
+
+            for (int y = 0; y < enemyCombatants.Count; y++)
+            {
+                if(enemyCombatants[y].Health > 0)
+                {
+                    var attackRoll = random.Next(0, 19) + enemyCombatants[y].AttackModifier;
+                    var randomAlly = allyCombatants[random.Next(0, allyCombatants.Count)];
+
+                    if (attackRoll >= randomAlly.ArmorClass)
+                    {
+                        randomAlly.Health -= enemyCombatants[y].AttackDamage;
+                        if (randomAlly.Health <= 0)
+                            Debug.Log($"{randomAlly.Name} has been slain!");
+                        else
+                            Debug.Log($"{randomAlly.Name} took {enemyCombatants[y].AttackDamage} damaage from {enemyCombatants[y].Name}! He now has {randomAlly.Health} health.");
+                    }
+                    else
+                        Debug.Log($"{randomAlly.Name} was not hit by {enemyCombatants[y].Name}");
+                }
+                else
+                    Debug.Log($"{enemyCombatants[y].Name} is slain...");
+            }
+
+            //TODO: Add these numbers and texts to the UI
         }
 
         void ClearStatFields()
@@ -206,6 +214,144 @@ namespace Assets.Scripts
             attackModifierTextField.GetComponent<InputField>().text = "";
             attackDamageTextField.GetComponent<InputField>().text = "";
             numberOfAttacksTextField.GetComponent<InputField>().text = "";
+        }
+
+        void SampleBattle()
+        {
+            Debug.Log("Kicking off a simple example battle...");
+
+            var ungor = new Combatant()
+            {
+                Name = "Ungor",
+                Enemy = false,
+                ArmorClass = 13,
+                Health = 15, //2d8+6
+                AttackModifier = 5,
+                AttackDamage = 9,
+                NumberOfAttacks = 1
+            };
+
+            var gor = new Combatant()
+            {
+                Name = "Gor",
+                Enemy = false,
+                ArmorClass = 13,
+                Health = 42, //5d8+20
+                AttackModifier = 6,
+                AttackDamage = 7,
+                NumberOfAttacks = 1
+            };
+
+            var bestigor = new Combatant()
+            {
+                Name = "Bestigor",
+                Enemy = false,
+                ArmorClass = 16,
+                Health = 93, //11d8+44
+                AttackModifier = 6,
+                AttackDamage = 15,
+                NumberOfAttacks = 1
+            };
+
+            var commoner = new Combatant()
+            {
+                Name = "Commoner",
+                Enemy = false,
+                ArmorClass = 10,
+                Health = 4, //1d4
+                AttackModifier = 2,
+                AttackDamage = 2,
+                NumberOfAttacks = 1
+            };
+
+            var guard = new Combatant()
+            {
+                Name = "Guard",
+                Enemy = false,
+                ArmorClass = 16,
+                Health = 11, //2d8+2
+                AttackModifier = 3,
+                AttackDamage = 4,
+                NumberOfAttacks = 1
+            };
+
+            var dretch = new Combatant()
+            {
+                Name = "Dretch",
+                Enemy = true,
+                ArmorClass = 11,
+                Health = 18, //4d6+4
+                AttackModifier = 2,
+                AttackDamage = 4,
+                NumberOfAttacks = 1
+            };
+
+            var maw = new Combatant()
+            {
+                Name = "Maw",
+                Enemy = true,
+                ArmorClass = 13,
+                Health = 33, //6d+6
+                AttackModifier = 4,
+                AttackDamage = 8,
+                NumberOfAttacks = 1
+            };
+
+            var hezrou = new Combatant()
+            {
+                Name = "Hezrou",
+                Enemy = true,
+                ArmorClass = 63,
+                Health = 136, //13d10+65
+                AttackModifier = 4,
+                AttackDamage = 8,
+                NumberOfAttacks = 3
+            };
+
+            allyCombatants.Add(ungor);
+            allyCombatants.Add(ungor);
+            allyCombatants.Add(ungor);
+            allyCombatants.Add(ungor);
+            allyCombatants.Add(ungor);
+            allyCombatants.Add(ungor);
+            allyCombatants.Add(ungor);
+            allyCombatants.Add(ungor);
+            allyCombatants.Add(ungor);
+            allyCombatants.Add(ungor);
+
+            allyCombatants.Add(gor);
+            allyCombatants.Add(gor);
+            allyCombatants.Add(gor);
+            allyCombatants.Add(gor);
+            allyCombatants.Add(gor);
+            allyCombatants.Add(gor);
+
+            allyCombatants.Add(bestigor);
+            allyCombatants.Add(bestigor);
+            allyCombatants.Add(bestigor);
+
+            enemyCombatants.Add(dretch);
+            enemyCombatants.Add(dretch);
+            enemyCombatants.Add(dretch);
+            enemyCombatants.Add(dretch);
+            enemyCombatants.Add(dretch);
+            enemyCombatants.Add(dretch);
+            enemyCombatants.Add(dretch);
+            enemyCombatants.Add(dretch);
+            enemyCombatants.Add(dretch);
+            enemyCombatants.Add(dretch);
+            enemyCombatants.Add(dretch);
+            enemyCombatants.Add(dretch);
+            enemyCombatants.Add(dretch);
+            enemyCombatants.Add(dretch);
+
+            enemyCombatants.Add(maw);
+            enemyCombatants.Add(maw);
+            enemyCombatants.Add(maw);
+            enemyCombatants.Add(maw);
+
+            enemyCombatants.Add(hezrou);
+            enemyCombatants.Add(hezrou);
         }
     }
 }
